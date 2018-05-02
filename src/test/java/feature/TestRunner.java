26fbe1d4a340c6f21d684dbaf4463b76ed92865d
Ocquidant Nicolas <nocquidant@voyages-sncf.com>
2018-05-02 11:26:59 +0200

    Trace for test runner

--- src/test/java/feature/TestRunner.java
+++ src/test/java/feature/TestRunner.java
@@ -4,15 +4,21 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.intuit.karate.cucumber.CucumberRunner;
 import com.intuit.karate.cucumber.KarateStats;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import cucumber.api.CucumberOptions;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
@@ -22,6 +28,19 @@ public class TestRunner {
 	static int KARATE_THREAD_COUNT = Integer.parseInt(System.getProperty("karate.thread.count", "1"));
 	static String KARATE_REPORT_PATH = System.getProperty("karate.report.path", "target");
 	static String KARATE_PROJECT_NAME = System.getProperty("karate.project.name", "testing-karate");
+
+	static org.slf4j.Logger logger = LoggerFactory.getLogger(TestRunner.class);
+
+	// We should probably use a logback.xml here
+	static {
+		Set<String> loggers = new HashSet<>(Arrays.asList("org.apache.http", "groovyx.net.http"));
+    
+    for(String log:loggers) { 
+    	Logger logger = (Logger)LoggerFactory.getLogger(log);
+    	logger.setLevel(Level.INFO);
+    	logger.setAdditive(false);
+		}
+	}
 
 	@Test
 	public void testParallel() {
@@ -36,6 +55,7 @@ public class TestRunner {
 		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
 		Configuration config = new Configuration(new File(KARATE_REPORT_PATH), KARATE_PROJECT_NAME);
 		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		logger.info("Generating report in: " + KARATE_REPORT_PATH);
 		reportBuilder.generateReports();
 	}
 }
