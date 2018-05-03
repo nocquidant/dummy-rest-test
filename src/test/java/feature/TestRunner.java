d948b82ffa03955a83b2600535ebf2da3ee65666
Ocquidant Nicolas <nocquidant@voyages-sncf.com>
2018-05-03 15:22:56 +0200

    Comment

--- src/test/java/feature/TestRunner.java
+++ src/test/java/feature/TestRunner.java
@@ -26,9 +26,8 @@ public class TestRunner {
 
 	@Test
 	public void testParallel() {
-		// TODO
-		// We could probably group features in packets and run only one packet given env variables
-		// ex. packet.current=3 and packet.max=10 --> execute in // the packet#3 with numFeatures/10 features
+		// TODO We could probably execute a group of features only, in order to distribute load and time on several containers
+		// ex. packet.current=3 and packet.max=10 with 100 features --> execute in // the packet#3 with numFeatures/10 in it
 		KarateStats stats = CucumberRunner.parallel(getClass(), env.karateThreadCount(), env.karateOutputPath());
 		ReportPublisher.generateReport(env);
 		assertTrue("there are scenario failures", stats.getFailCount() == 0);
